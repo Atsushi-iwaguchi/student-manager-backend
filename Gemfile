61cb64f9f0b0747cebea74cd225c4896e0255aca
Atsushi-iwaguchi <i.oneroom.bn@gmail.com>
2026-06-24 21:46:15 +0900

    change gemfile

--- Gemfile
+++ Gemfile
@@ -5,7 +5,9 @@ gem "rails", "~> 8.1.3"
 
 gem "pg", "~> 1.5"
 
-gem "devise"
+gem "bcrypt"
+gem "rack-cors"
+gem "jwt"
 
 # Use the Puma web server [https://github.com/puma/puma]
 gem "puma", ">= 5.0"
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,2 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  enum :role, { student: 0, teacher: 1 }
 end
