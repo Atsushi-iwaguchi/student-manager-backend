48ce28c49854f29763c2c6ac966a52a4962fad3e
Atsushi-iwaguchi <i.oneroom.bn@gmail.com>
2026-06-25 09:50:10 +0900

    applicationControllerとuserコントローラーの設定

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,27 @@
 class Api::V1::UsersController < ApplicationController
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+            token = generate_token(@user)
+            render json: {
+                user: { id: @user.id, email: @user.email, name: @user.name, role: @user.role },
+                token: token
+            }, status: :created
+        else
+            render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:email, :password, :password_confirmation, :name, :role, :target_school, :grade)
+    end
+
+    #user.to_token_payloadでペイロードを取得してJWT.encodeで秘密鍵を使って署名エンコードしてトークン文字列を生成
+    def generate_token(user)
+        payload = user.to_token_payload
+        JWT.encode(payload, Rails.application.credentials.secret_key_base, 'HS256')
+    end
+
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,39 @@
 class ApplicationController < ActionController::API
+    include ActionController::HttpAuthentication::Token::ControllerMethods
+
+    #今リクエストを送っているユーザーはだれかを返すメゾット
+    def current_user
+        #nilならauthenticate_tokenを読んで取得し、取得済みならキャッシュ値をそのまま使う
+        @current_user ||= authenticate_token
+    end
+
+    def logged_in?
+        #!!はnilでなければtrueを返す
+        !!current_user
+    end
+
+    #before_action :authenticate_user! と書くことでログインしていないユーザーのアクセスをはじく
+    def authenticate_user!
+        render json: { error: '認証が必要です' }, status: :unauthorized unless logged_in?
+    end
+
+    private
+    def authenticate_token
+        #Authorizationヘッダーからトークンを自動取得するメゾット
+        authenticate_with_http_token do |token, options|
+            begin
+                #JWT.decodeでトークンを検証, デコード
+                decoded = JWT.decoded(
+                    token,
+                    Rails.application.credentials.secret_key_base,
+                    true,
+                    { algorithm: 'HS256' }
+                )
+                user_id = decoded[0]["sub"]
+                User.find(user.id) #取り出したuser.idでDBからユーザーを検索しcurrent_userの返り値となる
+              rescue JWT::DecodeError, JWT::ExpiredSignature, ActiveRecord::RecordNotFound
+                nil
+            end
+        end
+    end
 end
